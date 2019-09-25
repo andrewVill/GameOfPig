13bce24e8dfaffffebc6c42fcc5b5b59f989047e
Andrew Villarreal <andrewvillarreal2010@hotmail.com>
2019-09-25 09:10:17 -0500

    Known Good

--- GameOfPig/ViewController.swift
+++ GameOfPig/ViewController.swift
@@ -9,12 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
     }
-
-
 }
 
