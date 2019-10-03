e32846a06ff29b7ab0f3e0dcbf3352d647df84d0
grace wall <grace.wall.154@k12.friscoisd.org>
2019-10-03 10:07:32 -0500

    fixing natural

--- GameOfPig/Natural.swift
+++ GameOfPig/Natural.swift
@@ -5,7 +5,7 @@
 //  Created by Villarreal, Andrew R on 9/25/19.
 //  Copyright © 2019 Villarreal, Andrew R. All rights reserved.
 //
-
+/*
 import Foundation
 
 var playerScores = Array(repeating: 0, count: 0)
@@ -56,4 +56,4 @@ func GOPPlayer(player: Int){
 }
 
 
-
+*/
