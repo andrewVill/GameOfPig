a7fdeff62ddbdb7b23331c375202f01fbfa94a40
grace wall <grace.wall.154@k12.friscoisd.org>
2019-10-03 09:26:17 -0500

    basically all running code

--- GameOfPig/Natural.swift
+++ GameOfPig/Natural.swift
@@ -7,45 +7,68 @@
 //
 
 import Foundation
-
-var playerScores = Array(repeating: 0, count: 0)
-var topPlayerScore = 0
+/*
+//var playerScores = Array(repeating: 0, count: 0)
+//var topPlayerScore = 0
 
 // this is an instalizer
 struct Natural{
-    let players = 0
+    let players = 2
+    var playerScores = [ 0, 0 ]
 //  let numOfDie = 0
-}
+    /*func setPlayer(){
+        playerScores = Array(repeating: 0, count: 0)
+    }*/
+    mutating func game(){
+        var i = 0
+        while(i < players){
+            var currentRoll = rollDice()
+            var addedScore = currentRoll
+            //var scoreBeforeAdded = playerScores[bigLoop]
+            //var plausibleTotal = (playerScores[bigLoop])+(addedScore)
+            print("you rolled a \(String(describing: currentRoll))")
+            //insert some way to find a boolean
+            if(currentRoll == 1){
+                addedScore = 0
+                break
+            }
+            var roll = true
+                while (roll){
+                    currentRoll = rollDice()
+                    if (currentRoll == 1){
+                        addedScore = 0
+                        roll = false
+                    }
+                    else{
+                        addedScore += currentRoll
+                        print("you rolled a \(String(describing: currentRoll))")//put into text label field
+                        //put hyppotheticalcurrent total into text field
+                        //reset roll boolean to see if wanna roll again - view controller button
+                    }
+                }
+                playerScores[i] += addedScore
+                if(playerScores[i] >= 100){
+                    //clear screen and say that they won
+                }
+                i += 1
+            }
+    }
+    func rollDice() -> Int{
+        //class GKRandomDistribution : NSObject
+        let random = Int.random(in: 1...6)
+        return random
+    }
+}*/
 
-func setPlayer(){
-    playerScores = Array(repeating: 0, count: 0)
-}
 
 //sets the score after turn() has ended
-func setPlayerScore(p:Int, score:Int){
+/*func setPlayerScore(p:Int, score:Int){
     playerScores[p] += score
-}
+}*/
 //returns a value that represents the random rolling of the die
-func rollDice() -> Int{
-    //class GKRandomDistribution : NSObject
-    let random = Int.random(in: 1...6)
-    return random
-}
-//is the loop for the turn
-func turn(player: Int, bigLoop: Int){
-    var currentRoll = rollDice
-    var addedScore = currentRoll
-    //var scoreBeforeAdded = playerScores[bigLoop]
-    //var plausibleTotal = (playerScores[bigLoop])+(addedScore)
-    print("you rolled a \(String(describing: currentRoll))")
-    //insert some way to find a boolean
-    var turn = true
-    var i = 0
-    //while (turn && players >= i){
-        
-    //}
 
-}
+//is the loop for the turn
+
 
 // This allows the game to run. You would call this in View Controller
 func GOPPlayer(player: Int){
